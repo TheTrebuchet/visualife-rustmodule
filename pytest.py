d81e628d5d145ac981d1c64e7cc95ac9496caf0e
TheTrebuchet <jan.kulczycki1@gmail.com>
2024-05-27 00:30:15 +0200

    the end of testing phase, shape=object approach works well

---
+++ pytest.py
@@ -0,0 +1,18 @@
+from svg_gen import Canvas, Circle
+import time
+
+canvas = Canvas()
+circles = []
+
+for i in range(10000):
+    radius = 10.0 + i
+    x = 5.0 * i
+    y = 5.0 * i
+    circle = Circle(radius, x, y)
+    circles.append(circle)
+
+stoper = time.time()
+result = canvas.generate_string()
+print(result)
+print(time.time()-stoper)
+
--- svg_gen/src/lib.rs
+++ svg_gen/src/lib.rs
@@ -1,25 +1,63 @@
 use pyo3::prelude::*;
+use std::fmt;
+use std::sync::Mutex;
+use lazy_static::lazy_static;
 
 #[pyclass]
-struct SvgViewport {
-    test_num: i32,
+struct Circle {
+    radius: f64,
+    x: f64,
+    y: f64,
 }
 
 #[pymethods]
-impl SvgViewport {
-    fn set_method(&mut self, value: i32) -> PyResult<()> {
-        self.test_num = value;
-        Ok(())
-    }
+impl Circle {
     #[new]
-    fn new(test_num: i32) -> Self {
-        SvgViewport { test_num: test_num }
+    fn new(radius: f64, x: f64, y: f64) -> Self {
+        let circ = Circle { radius, x, y };
+        register_shape(format!("{}", circ));
+        circ
+    }
+
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("Circle(radius: {}, x: {}, y: {})", self.radius, self.x, self.y))
     }
 }
 
-/// A Python module implemented in Rust.
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Circle(radius: {}, x: {}, y: {})", self.radius, self.x, self.y)
+    }
+}
+
+fn register_shape(shape: String) {
+    let mut shapes = SHAPES.lock().unwrap();
+    shapes.push(shape);
+}
+
+lazy_static! {
+    static ref SHAPES: Mutex<Vec<String>> = Mutex::new(Vec::new());
+}
+
+#[pyclass]
+struct Canvas;
+
+#[pymethods]
+impl Canvas {
+    #[new]
+    fn new() -> Self {
+        Canvas
+    }
+
+    fn generate_string(&self) -> PyResult<String> {
+        let shapes = SHAPES.lock().unwrap();
+        Ok(shapes.join(", "))
+    }
+}
+
 #[pymodule]
-fn svg_gen(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<SvgViewport>()?;
+fn svg_gen(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Circle>()?;
+    m.add_class::<Canvas>()?;
     Ok(())
 }
