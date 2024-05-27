b4e94694469cdf4527728b1aa8c4ff2caaaeeb01
TheTrebuchet <jan.kulczycki1@gmail.com>
2024-05-27 12:39:53 +0200

    now SHAPES contains actual objects with their methods

--- svg_gen/src/lib.rs
+++ svg_gen/src/lib.rs
@@ -1,42 +1,57 @@
 use pyo3::prelude::*;
-use std::fmt;
 use std::sync::Mutex;
 use lazy_static::lazy_static;
 
+pub trait Shape: Send {
+    fn as_any(&self) -> &dyn std::any::Any;
+    fn to_svg(&self) -> String;
+}
+
+impl Shape for Circle {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn to_svg(&self) -> String {
+        let mut svg_string = format!(
+            r#"<circle cx="{}" cy="{}" r="{}""#,
+            self.cx, self.cy, self.radius
+        );
+        svg_string.push_str(r#" />"#);
+        svg_string
+    }
+}
+
 #[pyclass]
+#[derive(Clone)]
 struct Circle {
     radius: f64,
-    x: f64,
-    y: f64,
+    cx: f64,
+    cy: f64,
 }
 
 #[pymethods]
 impl Circle {
     #[new]
-    fn new(radius: f64, x: f64, y: f64) -> Self {
-        let circ = Circle { radius, x, y };
-        register_shape(format!("{}", circ));
-        circ
+    fn new(radius: f64, cx: f64, cy: f64) -> Self {
+        let circle = Circle { radius, cx, cy };
+        register_shape(Box::new(circle.clone()));
+        circle
     }
 
     fn __repr__(&self) -> PyResult<String> {
-        Ok(format!("Circle(radius: {}, x: {}, y: {})", self.radius, self.x, self.y))
+        Ok(format!("Circle(radius: {}, cx: {}, cy: {})", self.radius, self.cx, self.cy))
     }
 }
 
-impl fmt::Display for Circle {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Circle(radius: {}, x: {}, y: {})", self.radius, self.x, self.y)
-    }
-}
-
-fn register_shape(shape: String) {
-    let mut shapes = SHAPES.lock().unwrap();
-    shapes.push(shape);
-}
 
 lazy_static! {
-    static ref SHAPES: Mutex<Vec<String>> = Mutex::new(Vec::new());
+    static ref SHAPES: Mutex<Vec<Box<dyn Shape>>> = Mutex::new(Vec::new());
+}
+
+fn register_shape(shape: Box<dyn Shape>) {
+    let mut shapes = SHAPES.lock().unwrap();
+    shapes.push(shape);
 }
 
 #[pyclass]
@@ -51,7 +66,8 @@ impl Canvas {
 
     fn generate_string(&self) -> PyResult<String> {
         let shapes = SHAPES.lock().unwrap();
-        Ok(shapes.join(", "))
+        let result = shapes.iter().map(|shape| shape.to_svg()).collect::<Vec<_>>().join("\n");
+        Ok(result)
     }
 }
 
