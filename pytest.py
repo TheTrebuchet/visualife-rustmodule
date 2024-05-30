0ec382b132d1bd8144d7d705a67500a40d98c962
TheTrebuchet <jan.kulczycki1@gmail.com>
2024-05-30 20:12:31 +0200

    new approach is working perfectly

--- pytest.py
+++ pytest.py
@@ -3,15 +3,16 @@ import time
 
 canvas = Canvas()
 circles = []
-
 for i in range(10000):
     radius = 10.0 + i
     x = 5.0 * i
     y = 5.0 * i
-    circle = Circle(radius, x, y)
+    circle = Circle('circle'+str(i), radius, x, y)
     circles.append(circle)
+    canvas.add_child(circle)
 
 stoper = time.time()
+for c in circles: c.cx = 0
 result = canvas.generate_string()
 print(result)
 print(time.time()-stoper)
--- svg_gen/src/lib.rs
+++ svg_gen/src/lib.rs
@@ -1,20 +1,42 @@
 use pyo3::prelude::*;
 use std::sync::{Arc, Mutex};
+use pyo3::types::PyAny;
 
 // here im trying to implement this idea, that in python
 // canvas = Canvas(...)
-// circle = Circle(canvas, ...)
+// circle = Circle(...)
+// canvas.add_child(circle)
 
-// so far it looks like I can't properly call a method of the python object and pass the newly created (for ex.) circle object to it
-// intuitively I see that passing the circle to it as the circle is being created is not really possible
-// like, this struct hasn't been converted to python object yet
+// we should totally call them students and scientists or something
 
-pub trait ToSvg: Send{
-    //fn as_any(&self) -> &dyn std::any::Any;
-    fn to_svg(&self) -> String;
+#[pyclass]
+struct Circle {
+    id: String,
+    #[pyo3(get, set)]
+    radius: f64,
+    #[pyo3(get, set)]
+    cx: f64,
+    #[pyo3(get, set)]
+    cy: f64,
+    children: Arc<Mutex<Vec<PyObject>>>
 }
 
-impl ToSvg for Circle {
+#[pymethods]
+impl Circle {
+    #[new]
+    fn new(_py: Python, id: String, radius: f64, cx: f64, cy: f64) -> Self {
+        let children = Arc::new(Mutex::new(Vec::new()));
+        let circle = Circle{id, radius, cx, cy, children};
+        circle
+    }
+    fn add_child(&self, py: Python, child: Py<PyAny>) {
+        let mut children = self.children.lock().unwrap();
+        children.push(child.clone_ref(py));
+        println!("child adopted")
+    }
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("Circle{}", self.id))
+    }
     fn to_svg(&self) -> String {
         let mut svg_string = format!(
             r#"<circle cx="{}" cy="{}" r="{}""#,
@@ -25,55 +47,6 @@ impl ToSvg for Circle {
     }
 }
 
-#[pyclass]
-struct Circle {
-    id: String,
-    radius: f64,
-    cx: f64,
-    cy: f64,
-    children: Arc<Mutex<Vec<PyObject>>>
-}
-
-#[pymethods]
-impl Circle {
-    #[new]
-    fn new(_py: Python, parent: &PyAny, id: String, radius: f64, cx: f64, cy: f64) -> Self {
-        let children = Arc::new(Mutex::new(Vec::new()));
-        let circle = Circle{id, radius, cx, cy, children};
-        let parent_ref: Py<PyAny> = parent.into();
-        //parent_ref.call_method1("add_child", (circle,)).unwrap(); ??????
-
-        circle
-    }
-    fn add_child(&self, py: Python, child: Py<PyAny>) {
-        let mut children = self.children.lock().unwrap();
-        children.push(child.clone_ref(py));
-    }
-    fn __repr__(&self) -> PyResult<String> {
-        Ok(format!("Circle{}", self.id))
-    }
-}
-
-pub trait AddChild: {
-    //fn as_any(&self) -> &dyn std::any::Any;
-    fn add_child(&self, child: Py<PyAny>) -> &Self;
-}
-
-impl AddChild for Circle {
-    fn add_child(&self, child: Py<PyAny>) -> &Self {
-        let mut children = self.children.lock().unwrap();
-        children.push(child);
-        self
-    }
-}
-
-impl AddChild for Canvas {
-    fn add_child(&self, child: Py<PyAny>) -> &Self {
-        let mut children = self.children.lock().unwrap();
-        children.push(child);
-        self
-    }
-}
 
 #[pyclass]
 struct Canvas {
@@ -87,10 +60,27 @@ impl Canvas {
         let children = Arc::new(Mutex::new(Vec::new()));
         Canvas { children }
     }
-
-    fn generate_string(&self) -> PyResult<String> {
-        let str = "svg".to_string();
-        Ok(str)
+    fn add_child(&self, py: Python, child: Py<PyAny>) {
+        let mut children = self.children.lock().unwrap();
+        children.push(child.clone_ref(py));
+        println!("child adopted")
+    }
+    fn generate_string(&self, py: Python) -> PyResult<String> {
+        let mut combined_svg = String::new();
+        let locked_children = self.children.lock().unwrap();
+        for obj in locked_children.iter() {
+            let py_any: &PyAny = obj.as_ref(py);
+            let result: Result<String, PyErr> = py_any.call_method0("to_svg").unwrap().extract();
+            let svg_string = match result {
+                Ok(svg) => svg,
+                Err(err) => {
+                    eprintln!("Error extracting SVG string: {}", err);
+                    continue; 
+                }
+            };
+            combined_svg.push_str(&svg_string)
+        };
+        Ok(combined_svg)
     }
 }
 
